90b4092b89a3a2ddac107df48b1cd609e775d630
Johannes Mols <johannes.mols@gmail.com>
2019-09-04 17:15:03 +0200

    Added code to compile both contracts

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -5,9 +5,26 @@ const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath); // delete build folder
 
-const electionPath = path.resolve(__dirname, 'contracts', 'Election.sol');
-const source = fs.readFileSync(electionPath, 'utf8');
-const output = solc.compile(source, 1).contracts;
+// Election.sol
+
+let electionPath = path.resolve(__dirname, 'contracts', 'Election.sol');
+let source = fs.readFileSync(electionPath, 'utf8');
+let output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath); // make sure that directory exists, creates it if it doesn't
+
+for (let contract in output) {
+    fs.outputJSONSync(
+        path.resolve(buildPath, contract.replace(':', '') + '.json'), // path to output file
+        output[contract] // output content
+    );
+}
+
+// RegistrationAuthority.sol
+
+electionPath = path.resolve(__dirname, 'contracts', 'RegistrationAuthority.sol');
+source = fs.readFileSync(electionPath, 'utf8');
+output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath); // make sure that directory exists, creates it if it doesn't
 
